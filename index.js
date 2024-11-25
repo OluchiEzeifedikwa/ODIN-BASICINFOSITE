c5b2ba1fd3672ab14334bad3e189c0acd1d09d69
OluchiEze <oluchiezeifedikwa@gmail.com>
2024-11-25 15:43:19 +0100

    Add index.js, package-lock.json and package.json current changes

--- index.js
+++ index.js
@@ -1,61 +1,13 @@
 const fs = require('fs');
 const http = require('node:http');
-<<<<<<< HEAD
 const path = require('path');
-=======
->>>>>>> 9ab15c118cba0019d6269ebe0a414f5daaf053cb
 const url = require('url');
 require("dotenv").config();
 const dotenv = require("dotenv").config();
 
 
-<<<<<<< HEAD
 
 
-=======
-fs.writeFile('index.html','!', err => {
-    if (err) {
-        console.error(err);
-    } else {
-        console.log('index.html Created');
-    }
-})
-
-
-fs.writeFile('about.html','!', err => {
-    if (err) {
-        console.error(err);
-    } else {
-        console.log('about.html Created');
-    }
-})
-
-fs.writeFile('contact-me.html','!', err => {
-    if (err) {
-        console.error(err);
-    } else {
-        console.log('contact-me.html Created');
-    }
-})
-
-fs.writeFile('404.html','!', err => {
-    if (err) {
-        console.error(err);
-    } else {
-        console.log('404.html Created');
-    }
-})
-
-
-
-fs.writeFile('index.js','', err => {
-    if (err) {
-        console.error(err);
-    } else {
-        console.log('index.js Created');
-    }
-})
->>>>>>> 9ab15c118cba0019d6269ebe0a414f5daaf053cb
 
 
 fs.writeFile('.env', '', err => {
@@ -77,7 +29,6 @@ fs.writeFile('.gitignore', '', err => {
 
 
 const server = http.createServer((req, res) => {
-<<<<<<< HEAD
 
     if (req.url === '/') {
         fs.readFile(path.join(__dirname, 'public', 'index.html'), (err, content) => {
@@ -107,13 +58,3 @@ const PORT = process.env.PORT || 5000;
 server.listen(PORT, () => console.log(`server listening on ${PORT}`)
 
 );
-=======
-    res.writeHead(200, {'Content-Type': 'Application/json'})
-    res.end(JSON.stringify(
-        {data: 'Hello WOrld'}
-    ))
-
-})
-
-server.listen(8000);
->>>>>>> 9ab15c118cba0019d6269ebe0a414f5daaf053cb
